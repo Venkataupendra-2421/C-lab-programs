302c4a556e117002d69935cb9a0b6545c1743e5e
Venkataupendra-2421 <112751688+Venkataupendra-2421@users.noreply.github.com>
2022-10-04 14:41:25 +0530

    Update D2 : Sum of fibonacci series.cpp

--- D2 : Sum of fibonacci series.cpp
+++ D2 : Sum of fibonacci series.cpp
@@ -1,17 +1,40 @@
-#include <iostream>  
-using namespace std;  
-int main() {  
-  int n1=0,n2=1,n3,i,n,sum=1;    
- cout<<"Enter the n value: ";    
- cin>>n;    
-  
- for(i=2;i<n;i++) 
- {  
-  n3=n1+n2;
-  sum=sum+n3;    
-  n1=n2;    
-  n2=n3;    
- }
- cout<<"sum of fibonacci series"<<sum;
- return 0;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int calculateEvenSum(int n)
+{
+	if (n <= 0)
+		return 0;
+
+	int fibo[2 * n + 1];
+	fibo[0] = 0, fibo[1] = 1;
+
+	
+	int sum = 0;
+
+	
+	for (int i = 2; i <= 2 * n; i++) {
+		fibo[i] = fibo[i - 1] + fibo[i - 2];
+        if (i % 2 == 0)
+			sum += fibo[i];
+	}
+
+	
+	return sum;
+}
+
+
+int main()
+{
+
+	
+	int n;
+    cout<<"Enter the value of n"<<endl;
+    cin>>n;
+
+	cout << " Sum of numbers at even indexes = "<< calculateEvenSum(n) << endl;
+
+	return 0;
 }
